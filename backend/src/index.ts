2b9c5a6de060e8d0765c9440d9c93a2db42bcb68
patha2702 <rajendrapatha2702@gmail.com>
2024-07-27 10:01:56 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express, {Request, Response} from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+const port= process.env.PORT || 3000
+
+app.listen(port, ()=> {
+    console.log(`Server running on port: ${port}`)
+})
